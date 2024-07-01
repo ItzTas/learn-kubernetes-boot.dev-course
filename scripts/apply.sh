ef8788dc15adada7027c6449c0998c904c90ad31
ItzTas <talessabini2@gmail.com>
2024-07-01 19:26:35 -0300

    crawler deploy updated

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -1,14 +1,19 @@
 cd learn-kubernetes-boot.dev-course/
 
+# configmap
 kubectl apply -f configmap/api-configmap.yaml 
 kubectl apply -f configmap/crawler-configmap.yaml 
+kubectl apply -f configmap/web-configmap.yaml 
 
+# deployment
 kubectl apply -f deployment/api-deployment.yaml 
 kubectl apply -f deployment/crawler-deployment.yaml 
 kubectl apply -f deployment/web-deployment.yaml 
 
+# ingress
 kubectl apply -f ingress/app-ingress.yaml 
 
+# service
 kubectl apply -f service/api-service.yaml 
 kubectl apply -f service/crawler-service.yaml 
 kubectl apply -f service/web-service.yaml 
