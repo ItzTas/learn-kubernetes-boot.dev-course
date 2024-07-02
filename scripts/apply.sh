a66b1f54f2c98b4a02aaecb859f4cf552ea6f703
ItzTas <talessabini2@gmail.com>
2024-07-02 01:00:25 -0300

    hpa study

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -4,11 +4,14 @@ cd learn-kubernetes-boot.dev-course/
 kubectl apply -f configmap/api-configmap.yaml 
 kubectl apply -f configmap/crawler-configmap.yaml 
 kubectl apply -f configmap/web-configmap.yaml 
+kubectl apply -f configmap/testram-configmap.yaml 
 
 # deployment
 kubectl apply -f deployment/api-deployment.yaml 
 kubectl apply -f deployment/crawler-deployment.yaml 
 kubectl apply -f deployment/web-deployment.yaml 
+kubectl apply -f deployment/testcpu-deployment.yaml
+kubectl apply -f deployment/testram-deployment.yaml
 
 # ingress
 kubectl apply -f ingress/app-ingress.yaml 
@@ -20,3 +23,6 @@ kubectl apply -f service/web-service.yaml
 
 # pvc
 kubectl apply -f pvc/api-pvc.yaml 
+
+# hpa
+kubectl apply -f hpa/testcpu-hpa.yaml 
